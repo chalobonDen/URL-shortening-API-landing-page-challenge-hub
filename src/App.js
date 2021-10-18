482153f20ed43be1b3f9f245a05f1eaaf2ad01e2
chalobon <chalobon-jaje@hotmail.com>
2021-10-18 14:51:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { memo, useState, createContext, useContext } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+const queryCilent = new QueryClient();
+
+const UrlContext = createContext();
+
+function UrlProvider({ children }) {
+  const [url, setUrl] = useState('https://www.frontendmentor.io/');
+
+  return (
+    <QueryClientProvider client={queryCilent}>
+      <UrlContext.Provider value={{ url, setUrl }}>
+        {children}
+      </UrlContext.Provider>
+    </QueryClientProvider>
+  );
+}
+
+const UrlContent = () => {
+  return (
+    <div className="App">
+      <URLinput />
+      <UrlResult />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UrlProvider>
+      <UrlContent></UrlContent>
+    </UrlProvider>
   );
 }
+
+// Component
+const URLinput = () => {
+  const { url, setUrl } = useContext(UrlContext);
+
+  return (
+    <select value={url} onChange={(event) => setUrl(event.target.value)}>
+      <option value="https://www.frontendmentor.io/">frontendmentor</option>
+      <option value="https://www.youtube.com/">youtube</option>
+      <option value="https://www.twitter.com/">twitter</option>
+    </select>
+  );
+};
+
+async function fetchUrl({ queryKey }) {
+  const response = await fetch(
+    `https://api.shrtco.de/v2/shorten?url=${queryKey}`
+  );
+  const data = await response.json();
+  return data;
+}
+
+const UrlResult = () => {
+  const { url } = useContext(UrlContext);
+  const { data, isLoading, error } = useQuery(url, fetchUrl);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Oops!! error occurred...</p>;
+
+  return (
+    <div>
+      <h1> {data.result.full_short_link} </h1>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      {/* <div className="row">
+        {data.message.map((val, index) => (
+          <img key={index} src={val} />
+        ))}
+      </div> */}
+
+      {console.log(data.result.full_short_link)}
+    </div>
+  );
+};
 
 export default App;
