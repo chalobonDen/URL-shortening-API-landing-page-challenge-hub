53ae513bc4dc8d2746a3922154cdcaa598f8967d
chalobon <chalobon-jaje@hotmail.com>
2021-10-19 11:12:26 +0700

    add header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Menu from './Menu';
+import working from '../../images/illustration-working.svg';
 
 const Header = () => {
   return (
     <div className="header">
       <Menu />
+      <div className="header-content">
+        <div className="title">
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <button>Get Started</button>
+        </div>
+        <img src={working} alt="" />
+      </div>
     </div>
   );
 };
