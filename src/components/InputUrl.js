4fd628de67faf2dbd2cfe7434a0e58a0e680a4eb
chalobon <chalobon-jaje@hotmail.com>
2021-10-21 23:07:09 +0700

    fixed bug

--- src/components/InputUrl.js
+++ src/components/InputUrl.js
@@ -1,74 +1,86 @@
 import { memo, useState, createContext, useContext } from 'react';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import './InputUrl.css';
 
-const queryCilent = new QueryClient();
+const InputUrl = () => {
+  const [linkContent, setLinkContent] = useState('');
+  const [error, setError] = useState('');
+  const [btnText, setBtnText] = useState('Shorten It!');
+  const [multipleShortLinks, setMultipleShortLinks] = useState([]);
+  const [copyState, setCopyState] = useState(true);
 
-const UrlContext = createContext();
+  const onLinkChange = (e) => {
+    setLinkContent(e.target.value);
+    setError('');
+  };
 
-function UrlProvider({ children }) {
-  const [url, setUrl] = useState('');
+  const handleShortLink = () => {
+    // e.preventDefault();
+    console.log(linkContent);
+    if (!linkContent) {
+      setError('Please add a link');
+    } else if (
+      !linkContent.includes('http') &&
+      !linkContent.includes('https') &&
+      !linkContent.includes('www')
+    ) {
+      setError('Please enter a valid URL');
+    } else {
+      getShortLink();
+      setLinkContent('');
+    }
+  };
 
-  return (
-    <QueryClientProvider client={queryCilent}>
-      <UrlContext.Provider value={{ url, setUrl }}>
-        {children}
-      </UrlContext.Provider>
-    </QueryClientProvider>
-  );
-}
-
-const UrlContent = () => {
-  return (
-    <div className="App">
-      <URLinput />
-      <UrlResult />
-    </div>
-  );
-};
-
-function InputUrl() {
-  return (
-    <UrlProvider>
-      <UrlContent></UrlContent>
-    </UrlProvider>
-  );
-}
-
-// Component
-const URLinput = () => {
-  const { url, setUrl } = useContext(UrlContext);
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={url}
-        onChange={(event) => setUrl(event.target.value)}
-      />
-    </div>
-  );
-};
-
-async function fetchUrl({ queryKey }) {
-  const response = await fetch(
-    `https://api.shrtco.de/v2/shorten?url=${queryKey}`
-  );
-  const data = await response.json();
-  return data;
-}
-
-const UrlResult = () => {
-  const { url } = useContext(UrlContext);
-  const { data, isLoading, error } = useQuery(url, fetchUrl);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Oops!! error occurred...</p>;
+  const getShortLink = async () => {
+    const longUrl = `https://api.shrtco.de/v2/shorten?url=${linkContent}`;
+    const existingLink = multipleShortLinks.find(
+      (shortLink) => shortLink['original_link'] === linkContent
+    );
+    if (!existingLink) {
+      setBtnText('Shortening...');
+      const response = await fetch(longUrl);
+      const fetchedShortUrl = await response.json();
+      if (fetchedShortUrl.ok) {
+        fetchedShortUrl.result['copy'] = true;
+        const newShortLink = [...multipleShortLinks, fetchedShortUrl.result];
+        setMultipleShortLinks(newShortLink);
+      } else {
+        setError('The short code API is unavailbale at the moment');
+      }
+      setBtnText('Shorten It!');
+    } else {
+      setError('You already have this link generated.');
+    }
+  };
 
   return (
-    <div>
-      {/* <h1> {data.result.full_short_link} </h1> */}
-      {data.ok === true ? <>{data.result.full_short_link}</> : <></>}
-    </div>
+    <main>
+      <div className="main-shortlink">
+        <div className="input-url">
+          <div>
+            <input
+              type="text"
+              value={linkContent}
+              onChange={(event) => onLinkChange(event)}
+              placeholder="Shorten a link here"
+            />
+            <button onClick={() => handleShortLink()}>Shorten it!</button>
+          </div>
+        </div>
+
+        {multipleShortLinks.map((shortLink, index) => (
+          <div key={index} className="link-url">
+            <div className="link-left">
+              <div className="full-link">{shortLink.original_link}</div>
+            </div>
+            <div className="link-right">
+              <div className="short-link">{shortLink.short_link}</div>
+              <button>Copy</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 };
 
