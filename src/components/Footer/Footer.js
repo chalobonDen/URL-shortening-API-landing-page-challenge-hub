570033dd6e0dc6a21b430e1eb7831416bbecdb70
chalobon <chalobon-jaje@hotmail.com>
2021-10-24 11:05:54 +0700

    add footer top component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FooterTop from './FooterTop';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <FooterTop />
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/FooterTop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const FooterTop = () => {
+  return (
+    <div className="footer-top">
+      <h1>Boost your links today</h1>
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default FooterTop;
